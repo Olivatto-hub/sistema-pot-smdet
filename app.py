1b2e41943cbcfcb829a38d54dfecb3207c859fe3
Olivatto-hub <rolivatto@prefeitura.sp.gov.br>
2025-12-09 18:49:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -507,4 +507,3 @@ if __name__ == "__main__":
     if 'logged_in' not in st.session_state: st.session_state['logged_in'] = False
     if st.session_state['logged_in']: app()
     else: login_screen()
-        
